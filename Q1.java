346c1dd150c6b9bf4565f8e0fe01345d2707eba1
Mario <mario.dsa123@gmail.com>
2021-08-27 11:06:33 +0530

    init

---
+++ Q1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Ran {
+
+    public static void main(String[] args) {
+        
+        List<String> argList = Arrays.asList(args);
+        Collections.shuffle(argList);
+        argList.stream()
+        .forEach(e->System.out.format("%s ",e));
+        for (String arg: argList) {
+            System.out.format("%s ", arg);
+        }
+
+        System.out.println();
+    }
+}
